a0b90eb6d57b08234c2fba9e027a1d5ae5209a3c
eli0009 <enlai.li@mail.mcgill.ca>
2023-10-19 12:43:54 -0400

    post request success

--- test_todo.py
+++ test_todo.py
@@ -1,17 +1,25 @@
+import json
 import unittest
 
 from helper_functions import *
 
 if __name__ == "__main__":
-    sendRequest("GET", "http://localhost:4567/todos", True)
-    sendRequest("HEAD", "http://localhost:4567/todos", True)
+    # r = requests.post("http://httpbin.org/post", json={"test": "cheers"})
+    # print(r.status_code)
 
-    data = """'title': "create paperwork"""
-    sendRequest(
+    # sendRequest("GET", "http://localhost:4567/todos", True)
+    # sendRequest("HEAD", "http://localhost:4567/todos", True)
+
+    data = {
+        "title": "officia deserunt mol",
+        "doneStatus": True,
+    }
+    r = sendRequest(
         "POST",
-        "http://localhost:4567/todos",
+        "todos",
         True,
         data,
     )
+    print(r.json().get("id"))
 
     # sendRequest("POST", "http://localhost:4567/todos", True, data)
